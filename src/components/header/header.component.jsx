9cc4be8b3bf977e8a8ca9bb6cc5fc8f838df76ad
EduardoVelarde <55457194+EduardoVelarde@users.noreply.github.com>
2021-09-30 18:47:41 -0500

    agregando store redux
    agregando store redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import './header.styles.scss'
 import{auth} from '../../firebase/firebase.utils';
+import {connect} from 'react-redux'
 
 import{ReactComponent as Logo} from '../../assets/crown.svg'
 const Header =({currentUser})=>(
@@ -27,6 +28,10 @@ const Header =({currentUser})=>(
         </Link>
         </div>
     </div>
-)
+);
 
-export default Header;
+const mapStateToProps = state=> ({
+    currentUser:state.user.currentUser
+})
+
+export default connect(mapStateToProps) (Header);
